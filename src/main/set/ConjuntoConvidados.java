c17106b80fbadbec90ca4f62d494833d4a75e03e
jackson grutka <jgrutka01@gmail.com>
2023-08-28 22:08:07 -0300

    operaçoes basicas com set java

---
+++ src/main/set/ConjuntoConvidados.java
@@ -0,0 +1,33 @@
+package main.set;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados(){
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidado(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for (Convidado c : convidadoSet) {
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+}
---
+++ src/main/set/Convidado.java
@@ -0,0 +1,39 @@
+package main.set;
+
+import java.util.Objects;
+
+public class Convidado {
+    private String nome;
+    private int codigoConvite;
+    
+    @Override
+    public String toString(){
+        return "nome: " + nome + ", Codigo do convite: " + codigoConvite + ".";
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (!(o instanceof Convidado convidado)) return false;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(getCodigoConvite());
+    }
+
+    public Convidado(String nome, int codigoConvite){
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    
+}
